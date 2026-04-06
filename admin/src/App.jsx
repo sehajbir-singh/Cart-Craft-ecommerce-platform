cffe312b79812c9d17428426e7d4004924c82af7
sehajbir-singh <sehajbirdhillon26@gmail.com>
2026-04-06 15:19:37 +0530

    completed Add Sidebar functionality and started List

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -41,9 +41,9 @@ const App = () => {
 
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path="/add" element={<Add />}></Route>
-                <Route path="/list" element={<List />}></Route>
-                <Route path="/orders" element={<Orders />}></Route>
+                <Route path="/add" element={<Add token={token}/>}></Route>
+                <Route path="/list" element={<List token={token}/>}></Route>
+                <Route path="/orders" element={<Orders token={token}/>}></Route>
               </Routes>
             </div>
           </div>
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-const list = () => {
-  return (
-    <div>
-      
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
+
+const list = ({ token }) => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      console.log();
+      if (response.data.success) {
+        console.log(response.data.products)
+        setList(response.data.products.reverse());
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return( <>
+  <p className="mb-2">All products list</p>
+  <div className="flex flex-col gap-2" >
+    <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 text-sm border bg-gray-100" >
+
+      <b>Image</b>
+      <b>Name</b>
+      <b>Category</b>
+      <b>Price</b>
+      <b className="text-center">Action</b>
+
     </div>
-  )
-}
 
-export default list
+    {/* Product list */}
+
+    {
+      list.map((item, index)=>{
+        <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] ">
+          <img src={item.image[0]} alt="" />
+          <p>{item.name}</p>
+          <p>{item.category}</p>
+          <p>{item.price}</p>
+          <p>X</p>
+
+        </div>
+      })
+    }
+
+
+  </div>
+
+  
+  </>
+    
+  );
+};
+
+export default list;
