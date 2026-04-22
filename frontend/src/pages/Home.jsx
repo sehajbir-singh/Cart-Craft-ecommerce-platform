0b44d3ad37245b9d5ce13226189e2eab1f9a4721
sehajbir-singh <sehajbirdhillon26@gmail.com>
2026-04-23 01:11:43 +0530

    Fixed name of a file

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
-import NewsLetterBox from '../components/NewsLetterBox'
+import NewsletterBox from '../components/NewsletterBox'
 
 
 const Home = () => {
@@ -13,7 +13,7 @@ const Home = () => {
       <LatestCollection/>
       <BestSeller/>
       <OurPolicy/>
-      <NewsLetterBox/>
+      <NewsletterBox/>
     </div>
   )
 }
